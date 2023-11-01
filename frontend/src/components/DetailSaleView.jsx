281e7d779c7a813498fc166e92586366a8263bdf
ET-SPARK <86984205+ET-SAPRK@users.noreply.github.com>
2023-11-01 22:35:36 +0300

    'edited'

--- frontend/src/components/DetailSaleView.jsx
+++ frontend/src/components/DetailSaleView.jsx
@@ -1,7 +1,101 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import {
+  faBed,
+  faBath,
+  faVectorSquare,
+  faWarehouse,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
 
 function DetailSaleView() {
-  return <div></div>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    // Fetch posts when the component mounts
+    async function fetchPosts() {
+      try {
+        const response = await axios.get("http://localhost:3000/sales");
+        setPosts(response.data);
+        console.log(posts);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    }
+
+    fetchPosts();
+  }, []);
+
+  const [expandedDescriptions, setExpandedDescriptions] = useState({});
+  return (
+    <div>
+      <Header />
+      <div class="grids">
+        {posts
+          .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+          .map((post) => (
+            <div key={post._id} class="single_grid">
+              <Link to={`/detail/${post._id}`} class="property_card">
+                <div className="post-container">
+                  <img className="grid_img" src={post.image} alt="Post" />
+                  <div className="overlay-text">
+                    <p>{post.status}</p>
+                  </div>
+                </div>
+                <div class="property-card__price">
+                  <span class="card__price_text">
+                    {" "}
+                    {post.price}&nbsp;br&nbsp; / &nbsp;month
+                  </span>
+                  <span class="property-card__summary card__price_text">
+                    {post.type},&nbsp; Renral Monthly
+                  </span>
+                  <span class="property-card__summary card__price_text">
+                    {post.place}
+                  </span>
+                </div>
+                <div class="property-card__details">
+                  <span class="property-card__heading">{post.title}</span>
+                  <span class="property-card__discription">
+                    {expandedDescriptions[post._id] ? (
+                      post.description
+                    ) : (
+                      <>
+                        {post.description.slice(0, 150)}
+                        {post.description.length > 150 && <span>...</span>}
+                      </>
+                    )}
+                  </span>
+                  <div class="property-card__features">
+                    <div class="property-card__features_bed">
+                      <FontAwesomeIcon icon={faBed} />
+                      &nbsp;
+                      {post.numberOfBed}&nbsp;
+                    </div>
+                    <div>
+                      <FontAwesomeIcon icon={faBath} />
+                      &nbsp; {post.numberOfBath}&nbsp;
+                    </div>
+                    <div class="property-card__features_bed">
+                      <FontAwesomeIcon icon={faVectorSquare} /> &nbsp;
+                      {post.propertySize}&nbsp; m<sup>2</sup>&nbsp;
+                    </div>
+                    <div>
+                      <FontAwesomeIcon icon={faWarehouse} />
+                      &nbsp; {post.numberOfGarage}&nbsp;
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))}
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default DetailSaleView;
