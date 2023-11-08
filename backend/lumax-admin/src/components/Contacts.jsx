ba4bae7d6fa050f63d26f096780bd4a6a197546c
ET-SPARK <86984205+ET-SAPRK@users.noreply.github.com>
2023-11-08 13:29:41 +0300

    edited

--- backend/lumax-admin/src/components/Contacts.jsx
+++ backend/lumax-admin/src/components/Contacts.jsx
@@ -1,7 +1,153 @@
 import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import "./Contacts.css";
 
 function Contacts() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <div className="grids">
+        <div className="stylish-box">
+          <div className="edit">
+            {" "}
+            <div>
+              <img src="https://cdn-icons-png.flaticon.com/512/3177/3177440.png" />
+            </div>
+            <div>
+              {" "}
+              <div>Name</div>
+              <div>Email</div>
+              <div>Phone</div>
+            </div>
+          </div>
+          <div className="info">
+            <div>Dropdown</div>
+            <div>Area</div>
+            <div>comment</div>
+          </div>
+          <div className="manipulate">
+            {" "}
+            <spna>add</spna>
+            <spna>remove</spna>
+          </div>
+        </div>
+        <div className="stylish-box">
+          <div>
+            {" "}
+            <img src="https://cdn-icons-png.flaticon.com/512/3177/3177440.png" />
+          </div>
+          <div className="info">
+            <div>Name</div>
+            <div>Email</div>
+            <div>Phone</div>
+            <div>Area</div>
+            <div>Comments</div>
+            <div>Dropdown</div>
+          </div>
+        </div>
+        <div className="stylish-box">
+          <div>
+            {" "}
+            <img src="https://cdn-icons-png.flaticon.com/512/3177/3177440.png" />
+          </div>
+          <div className="info">
+            <div>Name</div>
+            <div>Email</div>
+            <div>Phone</div>
+            <div>Area</div>
+            <div>Comments</div>
+            <div>Dropdown</div>
+          </div>
+        </div>
+        <div className="stylish-box">
+          <div>
+            {" "}
+            <img src="https://cdn-icons-png.flaticon.com/512/3177/3177440.png" />
+          </div>
+          <div className="info">
+            <div>Name</div>
+            <div>Email</div>
+            <div>Phone</div>
+            <div>Area</div>
+            <div>Comments</div>
+            <div>Dropdown</div>
+          </div>
+        </div>
+        <div className="stylish-box">
+          <div>
+            {" "}
+            <img src="https://cdn-icons-png.flaticon.com/512/3177/3177440.png" />
+          </div>
+          <div className="info">
+            <div>Name</div>
+            <div>Email</div>
+            <div>Phone</div>
+            <div>Area</div>
+            <div>Comments</div>
+            <div>Dropdown</div>
+          </div>
+        </div>
+        <div className="stylish-box">
+          <div>
+            {" "}
+            <img src="https://cdn-icons-png.flaticon.com/512/3177/3177440.png" />
+          </div>
+          <div className="info">
+            <div>Name</div>
+            <div>Email</div>
+            <div>Phone</div>
+            <div>Area</div>
+            <div>Comments</div>
+            <div>Dropdown</div>
+          </div>
+        </div>
+        <div className="stylish-box">
+          <div>
+            {" "}
+            <img src="https://cdn-icons-png.flaticon.com/512/3177/3177440.png" />
+          </div>
+          <div className="info">
+            <div>Name</div>
+            <div>Email</div>
+            <div>Phone</div>
+            <div>Area</div>
+            <div>Comments</div>
+            <div>Dropdown</div>
+          </div>
+        </div>
+        <div className="stylish-box">
+          <div>
+            {" "}
+            <img src="https://cdn-icons-png.flaticon.com/512/3177/3177440.png" />
+          </div>
+          <div className="info">
+            <div>Name</div>
+            <div>Email</div>
+            <div>Phone</div>
+            <div>Area</div>
+            <div>Comments</div>
+            <div>Dropdown</div>
+          </div>
+        </div>
+        <div className="stylish-box">
+          <div>
+            {" "}
+            <img src="https://cdn-icons-png.flaticon.com/512/3177/3177440.png" />
+          </div>
+          <div className="info">
+            <div>Name</div>
+            <div>Email</div>
+            <div>Phone</div>
+            <div>Area</div>
+            <div>Comments</div>
+            <div>Dropdown</div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default Contacts;
