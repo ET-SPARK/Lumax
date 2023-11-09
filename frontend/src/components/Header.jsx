04ac3a2a46a1cbbfef21b78ea1b150fb1952b568
ET-SPARK <86984205+ET-SAPRK@users.noreply.github.com>
2023-11-09 14:28:30 +0300

    edited

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -11,7 +11,7 @@ import { Link } from "react-router-dom";
 import Logo from "../images/LUMAX.png";
 function Header() {
   const [onPress, setOnPress] = useState(false);
-  const [onpressNav, setOnPressNav] = useState(true);
+  const [onpressNav, setOnPressNav] = useState(false);
   const [nav, setNav] = useState(false);
 
   const handleOnPress = () => {
@@ -43,7 +43,7 @@ function Header() {
           )}
         </div>
         {onpressNav && (
-          <div>
+          <div className="mobileHeader">
             <nav>
               <ul class="navList">
                 <li>
@@ -124,6 +124,86 @@ function Header() {
             </nav>
           </div>
         )}
+        <div className="DektopHeadet">
+          <nav>
+            <ul class="navList">
+              <li>
+                <Link to="/" class="link_style">
+                  Home
+                </Link>
+              </li>
+              <li class="non_link_hov">
+                <span
+                  onClick={() => {
+                    handleOnPress(setOnPress);
+                  }}
+                >
+                  {" "}
+                  Properties &nbsp;
+                  <FontAwesomeIcon icon={faCaretDown} />
+                  {nav && (
+                    <ul class="navList navList_Properties ">
+                      <li>
+                        <Link to="/for-sale" class="link_style">
+                          For Sale
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to="/for-rent" class="link_style">
+                          For Rent
+                        </Link>
+                      </li>
+                    </ul>
+                  )}
+                  {onPress && (
+                    <span className="prop prop_style">
+                      <span>
+                        <Link to="/for-sale" class="link_style">
+                          For Sale
+                        </Link>
+                      </span>
+                      <span>
+                        <Link to="/for-rent" class="link_style">
+                          For Rent
+                        </Link>
+                      </span>
+                    </span>
+                  )}
+                </span>
+              </li>
+              <li>
+                <Link to="/tools" class="link_style">
+                  Tools&nbsp; <FontAwesomeIcon icon={faCaretDown} />
+                </Link>
+              </li>
+              <li>
+                <Link to="/news" class="link_style">
+                  News
+                </Link>
+              </li>
+              <li>
+                <Link to="/about" class="link_style">
+                  About Us
+                </Link>
+              </li>
+              <li>
+                <Link to="/contact" class="link_style">
+                  Contact
+                </Link>
+              </li>
+              <li>
+                <Link to="/favourites" class="link_style">
+                  Favourites
+                </Link>
+              </li>
+              <li>
+                <Link to="/likes" class="link_style">
+                  <FontAwesomeIcon icon={faHeart} />
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
     </header>
   );
