5d22c39c3b12a60d76f1104dc66a47c61af99c32
ET-SPARK <86984205+ET-SAPRK@users.noreply.github.com>
2023-11-13 12:12:16 +0300

    edited

--- frontend/src/components/Privacy.jsx
+++ frontend/src/components/Privacy.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
+import "./Privacy.css";
 
 function Privacy() {
   return (
     <div>
       <Header />
+      <div className="section__background_overlay" />
+      <div>
+        <h1 className="section__heading_about">Privacy Policy</h1>
+        <div className="section_theme_lightest section__sub">
+          <div className="section__subheading">Commitment to your privacy</div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
