e13ac5f9b42bda9c2e719cc142bb8e3c94eb1d49
ET-SPARK <86984205+ET-SAPRK@users.noreply.github.com>
2023-11-13 11:53:40 +0300

    edited

---
+++ frontend/src/components/Victim.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+
+function Victim() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default Victim;
