dd8ce2f20e1d2dee2992e6df223351f87e828745
ET-SPARK <86984205+ET-SAPRK@users.noreply.github.com>
2023-11-13 13:40:41 +0300

    edited

--- frontend/src/components/Victim.jsx
+++ frontend/src/components/Victim.jsx
@@ -13,11 +13,211 @@ function Victim() {
           <div className="section__subheading">
             How exactly does the rental housing scam work?
           </div>
+          <div className="section__paragraph">
+            The criminals search websites and listings that list flats or homes
+            to let. They take the information in those ads and post it, with
+            their own e-mail address, in an ad on other websites or listings.
+          </div>
+          <div className="section__paragraph">
+            An interested person will contact this person via e-mail or
+            telephone. Victims are usually instructed to pay money over to them
+            before taking occupation. This amount will usually be for the first
+            month’s rental as well as a deposit.
+          </div>
+          <div className="section__paragraph">
+            The applicant might sometimes be asked to fill out an application
+            with bank account details, ID document copy and other personal
+            information. A Lease Agreement will also be signed but will be very
+            short documents will few details and few clauses.
+          </div>
           <div className="section__subheading">
             How do these criminals work?
           </div>
+          <div className="section__paragraph">
+            <ol>
+              <li>Intercepted Emails</li>
+              <div className="section__paragraph">
+                This involves scammers, hacking into the email of people
+                involved in the transactions, such as agents or lawyers, by
+                tricking home buyers into wiring funds to them instead of the
+                appropriate parties. They often will use a generic email address
+                indicating that the funds should be wired to a specific account
+                which will then vanish without a paper trail. Changes made
+                regarding payment details must always be done by the seller in
+                person if possible, or telephonically verifying the email, and
+                not via email alone.
+              </div>
+              <div className="section__paragraph">
+                Protect yourself by double checking everything, verifying all
+                emailed instructions that deal with the purchase even if they’re
+                from trusted parties, any bank detail changes need to be
+                accompanied by the required verification of the bank accounts in
+                question.
+              </div>
+              <li>Fraudsters posing as a Buyer</li>
+              <div className="section__paragraph">
+                They will approach a seller privately and show keen interest in
+                the property and put in an offer. After a few days, the supposed
+                buyer will contact the seller asking for a document to be signed
+                to help them get their home loan approved, which the seller then
+                signs without reading too much of the document only to discover
+                later that a third party claims to have bought the home. It will
+                be found that the scam artist (the first buyer) has been
+                marketing the home online as an agent, by taking the photos off
+                various websites, and has found a buyer who is also unaware that
+                something is wrong – and who might have paid a large deposit
+                over to the supposed agent. Make sure you check every detail
+                when it comes to property sale transactions, documents can be
+                falsified and email addresses cloned.
+              </div>
+              <li>Identity Theft</li>
+              <div className="section__paragraph">
+                Criminals have become much more experienced and are using stolen
+                identity details not only to empty bank accounts but to obtain
+                various credit accounts and even home loans. They are able to
+                delay detection of the fraud for long periods while the unpaid
+                bills and instalments mount up. The scammer will use false
+                documents to pose as the property owner, register forged
+                documents transferring a property to their name, and then get a
+                new mortgage against the property. After securing a mortgage or
+                line of credit, the criminal takes the cash and disappears.
+              </div>
+              <li>Bait and Switch Scheme</li>
+              <div className="section__paragraph">
+                This occurs when a prospective buyer offers an ‘above market
+                value’ price to a seller. The seller, impressed by the high
+                offer signs the contract, meanwhile the deceitful buyer has no
+                intention to purchase the property. Once the seller signs the
+                contract, the seller may only sell to that buyer for a specified
+                time, when that time ends the fraudster asks to extend the
+                contract a few weeks to work out closing details. Sounding
+                reasonable, the seller agrees to the extension blinded by the
+                high offer. In the meantime the seller keeps paying taxes,
+                maintenance, utilities and insurance the buyer comes back to the
+                seller with an excuse as to why this price no longer works, and
+                requests a reduction to below market value and threatens to
+                cancel if their demand is not met. Stressed by time and on-going
+                costs, the seller agrees to the reduction.
+              </div>
+              <li>Duplicated Listings</li>
+              <div className="section__paragraph">
+                ‘Agents’ copy legitimate rental listings and advertise for a
+                much cheaper price. Unfortunately, many people fall for these
+                fake listings and wire money to the owners of these fake
+                listings. When searching for a rental, do your research and make
+                sure you are working with a reputable company or agent.
+              </div>
+              <li>Fake Rental Agents</li>
+              <div className="section__paragraph">
+                When you find a property you really like, you call the agent to
+                arrange a viewing and they say they will meet you there. Later
+                they call and say they won’t be able to make it anymore, but no
+                need to worry the landlord will be there to show you around. The
+                agent then promises to negotiate a lower price with the
+                landlord. When you arrive at the house you find many other
+                people interested in renting the same place. You call the agent
+                back to negotiate a better price that you’re happy with; they
+                will phone you back shortly to inform you of the new price, all
+                you have to do is transfer the money for the first two months to
+                secure the place. On moving day, you find someone else is moving
+                in and the agent wasn’t an agent; they just found the property
+                online and reposted it with their own contact information. They
+                purposely send several people at a time to view the property to
+                generate a sense of urgency for the potential renters.
+              </div>
+            </ol>
+          </div>
           <div className="section__subheading">
             How to avoid being a victim of these scams:
+            <div className="section__paragraph">
+              <ul className="section__paragraph_li">
+                <li>
+                  Be wary when you are requested to make a payment for something
+                  minor like a credit check or security deposit, in most cases,
+                  there’s nothing you can do to get your money back because the
+                  scammer can’t be tracked.
+                </li>
+                <li>
+                  If the price looks too good to be true, it probably is. Prices
+                  are considerably higher than they were a few years ago.
+                </li>
+                <li>
+                  The email sounds strange – some listings hide the email
+                  address when you send a message, so you might not be able to
+                  see the address if you respond to the listing. Scammers
+                  usually use free email servers and they’ll often go by a
+                  series of random letters to make them less easily traceable.
+                </li>
+                <li>
+                  The Agent won’t show you the property – If you ask to see the
+                  property and they claim it’s impossible, it’s probably a fake
+                  listing. Agent will make time for people who are interested in
+                  the property.
+                </li>
+                <li>
+                  The seller pushes you – the faster a scammer gets you to agree
+                  to a business deal, the faster they can steal your money and
+                  avoid getting caught. The seller will often use high-pressure
+                  tactics that attempt to push you into acting quickly in order
+                  to purchase the home. Don’t be prodded by any seller to send
+                  money.
+                </li>
+                <li>
+                  The seller asks you to wire money – when you see the term
+                  “wire money” or similar variation of that phrase come up in a
+                  business conversation with someone you’ve never met, red flags
+                  should go up. Many scams entail wiring of funds because it’s
+                  more difficult to trace and enables the scammer to collect the
+                  money sooner. Scammers will come up with a variety of
+                  plausible reasons why the money should be wired rather than
+                  sent through a bank or lawyer.
+                </li>
+                <li>
+                  The buyer or seller is foreign and wants to buy a home unseen
+                  – most people want to at least see a property and become
+                  familiar with the area before making a large investment. This
+                  doesn’t mean you should be wary of all foreign inquiries, but
+                  many scams often occur overseas because it’s harder to trace
+                  the person behind the fraud. Foreign buyers who don’t ask
+                  questions, act in haste, and don’t care to see the property
+                  indicate a high likelihood of fraud.
+                </li>
+                <li>
+                  Be well informed about market related prices within the area
+                  you are looking to rent or buy. If a property is advertised
+                  way below the market related price for that area it should
+                  raise your concerns.
+                </li>
+                <li>
+                  If you found a “bargain” online you should call the estate
+                  agency to find out if the deal is for real. Don’t call the
+                  number at the bottom of the ad because this number could lead
+                  to a fake office. Rather find the actual office number, call
+                  there and ask the receptionist to give you the number of the
+                  specific agent or branch you are looking for.
+                </li>
+                <li>
+                  Be wary of agents and landlord who seem too eager or pushy to
+                  get you to live in their property or one they are marketing. A
+                  legit agent or landlord will always conduct the necessary
+                  checks and will not be too disappointed when you don’t show
+                  much interest in the property.
+                </li>
+                <li>
+                  If the agent is constantly making up excuses as to why they
+                  are not able to meet you or show you the property, you should
+                  also be worried. The chances are good that they don’t have
+                  access to the property and are stalling for time until they
+                  can think of a clever way to get you to pay the deposit.
+                </li>
+                <li>Never pay a deposit before you have viewed a property.</li>
+              </ul>
+            </div>
+            <div className="section__paragraph">
+              If you suspect a scam, have already been victimized, or know
+              someone who has fallen victim to a scam, please report it to your
+              local authority or police station.
+            </div>
           </div>
         </div>
       </div>
