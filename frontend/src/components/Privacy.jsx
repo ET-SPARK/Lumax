4649247529f76a541510f4cec1460f4f5ecfea2c
ET-SPARK <86984205+ET-SAPRK@users.noreply.github.com>
2023-11-13 11:52:27 +0300

    edited

---
+++ frontend/src/components/Privacy.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+
+function Privacy() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default Privacy;
