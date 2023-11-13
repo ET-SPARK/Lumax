81227791ba8cfefc1957835809323023e5b0ce84
ET-SPARK <86984205+ET-SAPRK@users.noreply.github.com>
2023-11-13 12:42:57 +0300

    edited

--- frontend/src/components/Privacy.jsx
+++ frontend/src/components/Privacy.jsx
@@ -93,6 +93,232 @@ function Privacy() {
               </li>
             </ul>
           </div>
+          <div className="section__subheading">Cookies</div>
+          <div className="section__paragraph">
+            The Website uses cookies in a limited way. Cookies are small files
+            containing information that a Website uses to track a visit by a
+            user. The Organisation uses session cookies to better understand how
+            the Website is used by users to improve the performance of the
+            Website for users, particularly the way search pages are delivered.
+            The Organisation has installed settings on the website to ensure
+            that session cookies do not remain on your computer at the end of
+            your visit to the Website, and cannot be used to obtain any
+            personally identifiable details.
+          </div>
+          <div className="section__subheading">Third-party websites</div>
+          <div className="section__paragraph">
+            The Website may contain links to third party websites. In the event
+            that you follow a link to any of these websites, it is important to
+            note that these websites have their own terms of use and privacy
+            policies and that the Organisation does not accept any
+            responsibility or liability for them. If you (i) are a client of the
+            Organisation, or (ii) are a user of the Website, and you have
+            purchased products or services from the Organisation, the
+            Organisation may use your contact details to send you details of any
+            new similar products or services which the Organisation thinks you
+            would be interested in. In doing so, the Organisation will at all
+            times comply with any applicable direct marketing laws. Any
+            communications that you do receive from the Organisation will set
+            out how to opt out of receiving future communications from the
+            Organisation, free of charge, if you no longer wish to receive
+            material for any reason whatsoever. The Organisation will only send
+            you marketing messages when you tick the relevant boxes at certain
+            times when engaging with the Organisation. As the Organisation is
+            not responsible for any representations, information, warranties and
+            / or content on any Website of any third party (including websites
+            linked to this Website), the Organisation does not exercise control
+            over third parties' privacy policies and the onus is on the User to
+            refer to the privacy policy of any such third party before providing
+            them with any of your Personal Information.
+          </div>
+          <div className="section__subheading">Use of the website</div>
+          <div className="section__paragraph">
+            By accessing the Website, you warrant and represent that (i) your
+            use of the website is for lawful purposes, (ii) you are over 18
+            (Eighteen) years of age, and (iii) you can legally conclude a
+            binding agreement with the Organisation. You further warrant that
+            you will not contravene any South African or international laws by
+            using the Website, any services offered on the Website or any
+            information provided to you by the Organisation through your use of
+            the Website. Except as expressly authorised by these Terms, you may
+            not use, alter, copy, distribute, or transmit any content contained
+            on this Website. The Organisation conducts its business in
+            accordance with all South African legislation applicable and
+            relevant to its business. One aspect of such legal compliance
+            pertains to data protection. The Organisation values the privacy of
+            your information and will protect your personal information in
+            accordance with all relevant laws and regulations. This includes the
+            Protection of Personal Information Act no 4 of 2013 (“POPIA”) and
+            all regulations promulgated in terms thereof. By using the Website,
+            and thereby accepting to be bound by these terms, you expressly
+            acknowledge, agree and consent to the Organisation and our
+            suppliers, or any person authorised on our behalf, using your
+            personal information, as this Term is defined by POPIA (“Personal
+            Information”) for any purpose necessary for you to use the Website,
+            or for the Organisation to render any service to you via the
+            Website.
+          </div>
+          <div className="section__subheading">Updating of privacy policy</div>
+          <div className="section__paragraph">
+            The Organisation, in its sole discretion, reserves the right to
+            update, modify or amend this Policy from time to time with or
+            without notice. You therefore agree and undertake to review the
+            Policy whenever you visit the Website. Save as expressly provided to
+            the contrary in this Policy, any amended version of the Policy shall
+            supersede and replace all previous versions thereof.
+          </div>
+          <div className="section__subheading">Amendment of terms</div>
+          <div className="section__paragraph">
+            The Organisation, in its sole discretion, reserves the right to
+            amend these Terms at any time. Any such amendments will come into
+            effect immediately and automatically. Whenever the Organisation
+            concludes any amendments to these Terms, the amended Terms will be
+            posted on this page, together with an indication at the bottom of
+            the page as to the date upon which the Terms were last revised. You
+            agree to review these Terms for any such amendments whenever you
+            visit the Website, and you will be deemed to have done so. Should
+            you not agree to any amendments of these Terms, you must immediately
+            cease to access and use the Website.
+          </div>
+          <div className="section__subheading">Content of users</div>
+          <div className="section__paragraph">
+            There are certain areas on the Website that allow users of the
+            Website to upload questions, data, and other information (“User
+            Content”). As a user, you are solely responsible for the User
+            Content that you upload, display, and / or otherwise make available
+            (“Add”) to the Website, and you hereby agree that the Organisation
+            is only a conduit for the online sharing, distribution and
+            publication of your User Content. The Organisation will not review
+            or reference any such User Content except as may be provided in the
+            Organisation’s privacy policy or as may be required by law. Unless
+            otherwise stipulated by law, or unless the contrary is specified in
+            any other Terms and conditions of the Organisation, all User Content
+            is owned by the User who adds it to the Website. Subject to the
+            aforegoing two provisos, the Organisation does not claim any
+            ownership rights in any User Content. You agree not to Add any User
+            Content that contains any information or content that (i) is not
+            legally permitted; (ii) you do not have a right to make available
+            under any law, or under contractual or fiduciary relationships; and
+            / or (iii) you know is incorrect and / or inaccurate. You agree that
+            any User Content that you Add to the Website does not and will not
+            violate any third-party rights of any kind.
+          </div>
+          <div className="section__subheading">
+            Copyright and intellectual property rights
+          </div>
+          <div className="section__paragraph">
+            For purposes of these Terms, “Intellectual Property Rights” means
+            “any and all Intellectual Property Rights of whatsoever nature and
+            form including, without limitation, patents, designs, copyright,
+            trademarks, trade secrets and / or know-how, whether in existence
+            now or whether arising in the future, and including all applications
+            and registrations, renewals and extensions thereof, under the laws
+            of any country, territory or other jurisdiction. Unless the contrary
+            is expressly specified in these Terms, any and all content and
+            material of whatsoever nature and form contained or depicted on the
+            Website, or incorporated or embedded in any service offered on or
+            via the Website, including, without limitation software, images,
+            text, graphics, illustrations, logos, branding, and/ or photographs
+            (“Organisation Content”), and all Intellectual Property Rights
+            subsisting in such Organisation Content, vest exclusively in the
+            Organisation. You hereby agree and undertake that you will at no
+            time lay any claim to the Organisation Content, and / or to any
+            Intellectual Property Rights subsisting in such Organisation
+            Content. Except as explicitly provided herein, nothing in these
+            Terms shall be deemed to create a license in or to any Intellectual
+            Property Rights belonging to the Organisation, and you agree and
+            undertake that you will not yourself, or through any parent,
+            subsidiary, affiliate, agent or other third party: (i) modify, port,
+            translate, localise or create derivative works of the Organisation
+            Content, or any part thereof; (ii) by any means whatsoever,
+            decompile, disassemble, reverse engineer or attempt to reconstruct,
+            identify or discover any source code, underlying ideas, underlying
+            user interface techniques or algorithms contained or incorporated in
+            any Organisation Content, or any part thereof, , or disclose any of
+            the foregoing; (iii) sell, lease, license, sublicense, copy, market,
+            reproduce, transmit or distribute the Organisation Content, or any
+            part thereof, including any Intellectual Property Rights subsisting
+            therein, without the express written consent of the Organisation;
+            (iv) encumber or create any lien or security interest in respect of
+            any Organisation Content or any Intellectual Property Rights
+            subsisting therein; and (v) knowingly take any action that would
+            cause any of the Organisation Content to be placed in the public
+            domain, or that would infringe any Intellectual Property Rights in
+            the Organisation Content. Use of the Organisation Content, and any
+            Intellectual Property Rights subsisting therein, for any purpose not
+            expressly permitted by these Terms is strictly prohibited. You
+            understand and acknowledge that you may be exposed to User Content
+            that is inaccurate, misleading and / or offensive and you agree that
+            the Organisation will not be liable for any damages you allege to
+            incur as a result of exposure to such User Content.
+          </div>
+          <div className="section__subheading">
+            Disclaimer of warranties and liabilities
+          </div>
+          <div className="section__paragraph">
+            The Organisation does not make any warranties, representations,
+            statements or guarantees, whether express, implied in law or
+            residual, regarding the Website and any services offered on or via
+            the Website, and these are provided on an "as is" basis. The
+            Organisation also does not make any warranty or representation that
+            any information contained on the Website or included in any services
+            is appropriate for use in any jurisdiction. Use of the Website, any
+            Organisation Content and / or any service offered as a part thereof
+            is entirely at your own risk. The Organisation makes no
+            representations, warranties or conditions about the quality,
+            accuracy, reliability, completeness, or timeliness of any of the
+            aforegoing. The Organisation does not assume any responsibility for
+            any errors, omissions or inaccuracies in the Website, the
+            Organisation Content and / or any service that may be offered
+            pursuant thereto. Neither the Organisation nor its shareholders,
+            officers, directors, employees, affiliate companies, agents and
+            advisors (“Indemnified Parties”) shall be responsible for, and deny,
+            all liability for any loss, harm, damage (whether direct, indirect
+            or consequential) and / or expense of any nature whatsoever which
+            may be suffered by you and / or any third party, as a result of or
+            which may be attributable, directly or indirectly, to your access
+            and use of (i) the Website, and / or (ii) any information contained
+            on or received via the Website (including any reliance you may place
+            on such information), (iii) any Organisation Content, (iv) any
+            services provided pursuant to the Website. Without limiting the
+            generality of the aforegoing, the Indemnified Parties shall not be
+            liable for any (i) loss of business, data and / or profits, (ii)
+            failure and / or unavailability of the Website for any reason
+            whatsoever, and / or (iii) failure / delay by any third party
+            service provider to render any service/s which are necessary to
+            ensure the availability of the Website. You hereby indemnify the
+            Indemnified Parties against any loss, liability, harm, damage
+            (whether direct, indirect or consequential) or expense of any nature
+            whatsoever which may be suffered by you or any third party as a
+            result of or which may be attributable directly or indirectly to any
+            of the above or the aforegoing.
+          </div>
+          <div className="section__subheading">Indemnity</div>
+          <div className="section__paragraph">
+            In addition to the warranties and indemnities set out above, you
+            hereby agree and undertake to defend, indemnify and hold harmless
+            the Indemnified Parties from and against any and all claims,
+            damages, obligations, losses, liabilities, costs or debt, and
+            expenses (including but not limited to attorneys' fees) arising
+            from: (i) your violation of any provision of these Terms; (iii) your
+            violation of any third party right including, without limitation any
+            Intellectual Property Right, or other property or privacy right; or
+            (iv) any claim that the User Content caused damage to a third party.
+            All indemnity provisions set out in these Terms will survive
+            termination, modification or expiration of these Terms.
+          </div>
+          <div className="section__subheading">Governing law</div>
+          <div className="section__paragraph">
+            These Terms and all matters or disputes arising from or incidental
+            to them shall be governed and construed in accordance with the laws
+            of the Republic of South Africa and you hereby submit to the
+            exclusive jurisdiction of the South African courts. If any provision
+            of these Terms is found to be unlawful, void, or for any reason
+            unenforceable by a competent court in the Republic of South Africa
+            then that provision shall be severable from these Terms and shall
+            not affect the validity and enforceability of any remaining
+            provisions.
+          </div>
         </div>
       </div>
       <Footer />
