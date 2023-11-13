564ee2e881ba4bcfbffbd8aff4d9fab1620de6ba
ET-SPARK <86984205+ET-SAPRK@users.noreply.github.com>
2023-11-13 13:26:54 +0300

    edited

--- frontend/src/components/Victim.jsx
+++ frontend/src/components/Victim.jsx
@@ -6,6 +6,21 @@ function Victim() {
   return (
     <div>
       <Header />
+      <div className="section__background_overlay" />
+      <div>
+        <h1 className="section__heading_about">Don't Be A Victim Of Scams</h1>
+        <div className="section_theme_lightest section__sub">
+          <div className="section__subheading">
+            How exactly does the rental housing scam work?
+          </div>
+          <div className="section__subheading">
+            How do these criminals work?
+          </div>
+          <div className="section__subheading">
+            How to avoid being a victim of these scams:
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
