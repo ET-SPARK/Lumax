276d8e2443aa703502ebecdef1def840c38690f3
ET-SPARK <86984205+ET-SAPRK@users.noreply.github.com>
2023-11-14 19:01:34 +0300

    edited

--- backend/lumax-admin/src/components/News.jsx
+++ backend/lumax-admin/src/components/News.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
 
 function News() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
 }
 
 export default News;
