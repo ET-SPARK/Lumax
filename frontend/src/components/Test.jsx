1af8b6c168e45ef2607fb901556258a2138337db
ET-SPARK <86984205+ET-SAPRK@users.noreply.github.com>
2023-11-17 15:10:20 +0300

    edited

---
+++ frontend/src/components/Test.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import "./Test.css";
+
+function Test() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default Test;
