7f0572af60dd0ef9cb5e76ff511e48cc6bdfaf5c
ET-SAPRK <samuelwoyesso2016@gmail.com>
2023-11-28 01:10:06 -0800

    edited

--- frontend/src/components/Detail.jsx
+++ frontend/src/components/Detail.jsx
@@ -3,13 +3,47 @@ import Footer from "./Footer";
 import Header from "./Header";
 import "./Detail.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faXmark, faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import {
+  faXmark,
+  faLocationDot,
+  faBed,
+  faBath,
+  faArrowRight,
+  faVectorSquare,
+  faWarehouse,
+} from "@fortawesome/free-solid-svg-icons";
 import { useParams, Link } from "react-router-dom";
 import axios from "axios";
 import ReCAPTCHA from "react-google-recaptcha";
 import Modal from "react-modal";
 
 function Detail() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    // Fetch posts when the component mounts
+    async function fetchPosts() {
+      try {
+        const response = await axios.get("http://localhost:3000/posts");
+        setPosts(response.data);
+        console.log(posts);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    }
+
+    fetchPosts();
+  }, []);
+
+  const [expandedDescriptions, setExpandedDescriptions] = useState({});
+
+  const toggleDescription = (postId) => {
+    setExpandedDescriptions((prev) => ({
+      ...prev,
+      [postId]: !prev[postId],
+    }));
+  };
+
   const [onPress, setOnPress] = useState(false);
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
@@ -433,6 +467,69 @@ function Detail() {
           </span>
         </div>
       </form>
+      <div class="grids">
+        {posts
+          .filter((post) => post._id !== postId)
+          .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+          .slice(0, 3) // Get only the first 3 posts
+          .map((post) => (
+            <div key={post._id} class="single_grid">
+              <Link to={`/detail/${post._id}`} class="property_card">
+                <div className="post-container">
+                  <img className="grid_img" src={post.image} alt="Post" />
+                  <div className="overlay-text">
+                    <p>{post.status}</p>
+                  </div>
+                </div>
+                <div class="property-card__price">
+                  <span class="card__price_text">
+                    {" "}
+                    {post.price}&nbsp;br&nbsp; / &nbsp;month
+                  </span>
+                  <span class="property-card__summary card__price_text">
+                    {post.type},&nbsp;
+                  </span>
+                  <span class="property-card__summary card__price_text">
+                    {post.place}
+                  </span>
+                </div>
+                <div class="property-card__details">
+                  <span class="property-card__heading">{post.title}</span>
+                  <span class="property-card__discription">
+                    {expandedDescriptions[post._id] ? (
+                      post.description
+                    ) : (
+                      <>
+                        {post.description.slice(0, 150)}
+                        {post.description.length > 150 && <span>...</span>}
+                      </>
+                    )}
+                  </span>
+                  <div class="property-card__features">
+                    <div class="property-card__features_bed">
+                      <FontAwesomeIcon icon={faBed} />
+                      &nbsp;
+                      {post.numberOfBed}&nbsp;
+                    </div>
+                    <div>
+                      <FontAwesomeIcon icon={faBath} />
+                      &nbsp; {post.numberOfBath}&nbsp;
+                    </div>
+                    <div class="property-card__features_bed">
+                      <FontAwesomeIcon icon={faVectorSquare} />
+                      &nbsp;
+                      {post.propertySize}m<sup>2</sup>&nbsp;
+                    </div>
+                    <div>
+                      <FontAwesomeIcon icon={faWarehouse} />
+                      &nbsp;{post.numberOfGarage}&nbsp;
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))}
+      </div>
       <Footer />
     </div>
   );
