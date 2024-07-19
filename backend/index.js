3b83f65f3dc3cf733b6ae24bc3985581152bf4b9
Samuel Woyesso <samuelwoyesso2016@gmail.com>
2024-07-19 11:08:04 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const port = 3000;
 const cors = require("cors");
 app.use(
   cors({
-    origin: ["https://lumax-test-jih4.vercel.app"],
+ origin: ["http://localhost:5173/"],
     methods: ["POST", "GET"],
     credentials: true,
   })
